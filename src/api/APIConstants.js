48a3cc58e2b3028c62eae6b384102f3e49edb56e
joelwright-atio <89510337+joelwright-atio@users.noreply.github.com>
2023-08-01 14:44:05 +0200

    reset

--- src/api/APIConstants.js
+++
@@ -1,4 +0,0 @@
-const domain = "http://";
-const address = "localhost:";
-const port = "8080/";
-export const FullAddress = domain + address + port;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "react-widgets/styles.css";
-import "./i18next";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Suspense fallback={<div>Loading ~~~</div>}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Suspense>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
